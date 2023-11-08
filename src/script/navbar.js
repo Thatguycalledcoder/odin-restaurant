8f3830e71e846e53e478b2dc8af9896c76a5b198
Thatguycalledcoder <quarshiedavid62@gmail.com>
2023-11-07 18:11:28 -0600

    Added script for creating navbar with DOM manipulation.

---
+++ src/script/navbar.js
@@ -0,0 +1,30 @@
+export default function Navbar(link = "Home") {
+    const header = document.createElement("header");
+
+    const h1 = document.createElement("h1");
+    h1.id = "logo";
+    h1.textContent = "Nappy's";
+    header.appendChild(h1);
+
+    const nav = document.createElement("nav");
+    const ul = document.createElement("ul");
+
+    for (const item of ["Home", "Menu", "Contact"]) {
+        const li = document.createElement("li");
+        li.id = item.toLowerCase();
+        li.textContent = item;
+        if (item == link) {
+            li.classList.add("active");
+        }
+        else {
+            li.classList.remove("active");
+        }
+        ul.appendChild(li);
+    }
+
+    nav.appendChild(ul);
+    header.appendChild(nav);
+
+    const div = document.createElement("div");
+    header.appendChild(div);
+}
